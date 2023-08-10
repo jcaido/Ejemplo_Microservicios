65845ed7d0e0f0b0f447b0e3fbf693ffc4bf893a
Cristobal Rosa <crisrogar@gmail.com>
2023-08-10 19:43:10 +0200

    Initial commit

---
+++ microservicio_ejemplo/src/test/java/com/jcaido/microservicio_ejemplo/MicroservicioEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcaido.microservicio_ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
