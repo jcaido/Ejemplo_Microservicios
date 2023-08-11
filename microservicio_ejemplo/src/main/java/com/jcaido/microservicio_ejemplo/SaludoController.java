17ff6c782dbbe150a6dabdbafc60efd68c6204cb
Cristobal Rosa <crisrogar@gmail.com>
2023-08-11 17:47:56 +0200

    feat: saludo personalizado SaludoController microservicio ejemplo

--- microservicio_ejemplo/src/main/java/com/jcaido/microservicio_ejemplo/SaludoController.java
+++ microservicio_ejemplo/src/main/java/com/jcaido/microservicio_ejemplo/SaludoController.java
@@ -2,6 +2,7 @@ package com.jcaido.microservicio_ejemplo;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,11 @@ public class SaludoController {
     @GetMapping(value = "saludo", produces = MediaType.TEXT_PLAIN_VALUE)
     public String saludo() {
         return "Microservicio Spring Boot";
+    }
+
+    @GetMapping(value = "saludo/{name}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String saludo(@PathVariable("name") String nombre) {
+
+        return "Bienvenido a Spring boot Sr./a " + nombre;
     }
 }
