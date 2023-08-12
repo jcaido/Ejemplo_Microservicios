ef26568aed6c1e12d8016d80562b79302d321638
Cristobal Rosa <crisrogar@gmail.com>
2023-08-12 18:37:05 +0200

    feat: creacion microservicio cursos-crud

---
+++ microservicio_cursos - crud/src/main/java/com/jcaido/microservicio_cursos/MicroservicioCursosApplication.java
@@ -0,0 +1,13 @@
+package com.jcaido.microservicio_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCursosApplication.class, args);
+	}
+
+}
---
+++ microservicio_cursos - crud/src/main/java/com/jcaido/microservicio_cursos/controller/CursoController.java
@@ -0,0 +1,49 @@
+package com.jcaido.microservicio_cursos.controller;
+
+import com.jcaido.microservicio_cursos.model.Curso;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CursoController {
+
+    private List<Curso> cursos = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        cursos.add(new Curso("Spring", 40, "mañana"));
+        cursos.add(new Curso("Spring boot", 150, "tarde"));
+        cursos.add(new Curso("Spring boot", 150, "tarde"));
+        cursos.add(new Curso("PhP", 50, "tarde"));
+        cursos.add(new Curso("Node.js", 15, "mañana"));
+
+    }
+
+    @GetMapping(value = "curso", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Curso getCurso() {
+        return new Curso("Java", 20, "mañana");
+    }
+
+    @GetMapping(value = "cursos", produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Curso> cursos() {
+        return cursos;
+    }
+
+    @GetMapping(value = "curso/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Curso> buscarCurso(@PathVariable("name") String nombre) {
+        List<Curso> aux = new ArrayList<>();
+        for (Curso curso: cursos) {
+            if (curso.getNombre().contains(nombre)) {
+                aux.add(curso);
+            }
+        }
+
+        return aux;
+    }
+}
