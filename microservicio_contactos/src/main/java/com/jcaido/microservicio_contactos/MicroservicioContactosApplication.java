cbf18686c1d6bc5407d95f98d6f95f6bd5497aa5
Cristobal Rosa <crisrogar@gmail.com>
2023-08-13 10:32:56 +0200

    feat: creacion microservicio contactos

---
+++ microservicio_contactos/src/main/java/com/jcaido/microservicio_contactos/MicroservicioContactosApplication.java
@@ -0,0 +1,13 @@
+package com.jcaido.microservicio_contactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioContactosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioContactosApplication.class, args);
+	}
+
+}
