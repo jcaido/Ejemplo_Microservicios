88086a0640cc27c341af7c9f0b02b88bfd77694e
Cristobal Rosa <crisrogar@gmail.com>
2023-08-14 10:21:18 +0200

    feat: recuperar contactos en controller microservicio-contactos

--- microservicio_contactos/src/main/java/com/jcaido/microservicio_contactos/controller/ContactoController.java
+++ microservicio_contactos/src/main/java/com/jcaido/microservicio_contactos/controller/ContactoController.java
@@ -2,11 +2,15 @@ package com.jcaido.microservicio_contactos.controller;
 
 import com.jcaido.microservicio_contactos.model.Contacto;
 import com.jcaido.microservicio_contactos.service.ContactoService;
+import jakarta.persistence.GeneratedValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ContactoController {
@@ -17,5 +21,10 @@ public class ContactoController {
     @PostMapping(value = "contactos", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
     public String guardarContacto(@RequestBody Contacto contacto) {
         return String.valueOf(contactoService.agregarContacto(contacto));
+    }
+
+    @GetMapping(value = "contactos", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Contacto> obtenerTodosLosContactos() {
+        return contactoService.recuperarContactos();
     }
 }
