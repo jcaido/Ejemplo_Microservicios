bdc7e4ea465d4770c8fb6dbf9bec858e1cd32348
Cristobal Rosa <crisrogar@gmail.com>
2023-08-15 13:16:44 +0200

    feat: creacion java bean RestTemplate

--- microservicio_cliente_contactos/src/main/java/com/jcaido/microservicio_cliente_contactos/MicroservicioClienteContactosApplication.java
+++ microservicio_cliente_contactos/src/main/java/com/jcaido/microservicio_cliente_contactos/MicroservicioClienteContactosApplication.java
@@ -2,12 +2,20 @@ package com.jcaido.microservicio_cliente_contactos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MicroservicioClienteContactosApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MicroservicioClienteContactosApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate template() {
+		return new RestTemplate();
 	}
 
 }
