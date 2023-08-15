a0c37e9ad850847213d86419a364d0b067d6c1e6
Cristobal Rosa <crisrogar@gmail.com>
2023-08-15 10:58:29 +0200

    feat: crear microservicio_cliente del microservicio contactos

---
+++ microservicio_cliente_contactos/src/main/java/com/jcaido/microservicio_cliente_contactos/MicroservicioClienteContactosApplication.java
@@ -0,0 +1,13 @@
+package com.jcaido.microservicio_cliente_contactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioClienteContactosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioClienteContactosApplication.class, args);
+	}
+
+}
