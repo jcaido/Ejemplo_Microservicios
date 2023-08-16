8013d9e909753001f6e25be0c1c6fa435a667d7d
Cristobal Rosa <crisrogar@gmail.com>
2023-08-16 13:46:23 +0200

    feat: configuracion EnableAsync, bean executor

--- microservicio_cliente_contactos_asincrono/src/main/java/com/jcaido/microservicio_cliente_contactos/MicroservicioClienteContactosApplication.java
+++ microservicio_cliente_contactos_asincrono/src/main/java/com/jcaido/microservicio_cliente_contactos/MicroservicioClienteContactosApplication.java
@@ -3,9 +3,14 @@ package com.jcaido.microservicio_cliente_contactos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.Executor;
+
 @SpringBootApplication
+@EnableAsync
 public class MicroservicioClienteContactosApplication {
 
 	public static void main(String[] args) {
@@ -16,6 +21,11 @@ public class MicroservicioClienteContactosApplication {
 	@Bean
 	public RestTemplate template() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public Executor executor() {
+		return new ThreadPoolTaskExecutor();
 	}
 
 }
