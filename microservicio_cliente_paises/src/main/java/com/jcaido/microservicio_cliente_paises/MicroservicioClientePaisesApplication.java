5467bdf36f95751ae7133943a24bd8fa8157bf4b
Cristobal Rosa <crisrogar@gmail.com>
2023-08-18 13:52:29 +0200

    feat: configuracion bean RestTemplate

--- microservicio_cliente_paises/src/main/java/com/jcaido/microservicio_cliente_paises/MicroservicioClientePaisesApplication.java
+++ microservicio_cliente_paises/src/main/java/com/jcaido/microservicio_cliente_paises/MicroservicioClientePaisesApplication.java
@@ -2,12 +2,20 @@ package com.jcaido.microservicio_cliente_paises;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MicroservicioClientePaisesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MicroservicioClientePaisesApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate template() {
+		return new RestTemplate();
 	}
 
 }
