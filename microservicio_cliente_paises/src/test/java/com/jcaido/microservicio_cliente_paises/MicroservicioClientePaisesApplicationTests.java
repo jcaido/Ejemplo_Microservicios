403fd38556ed3de5e3efdeadcc048aa2d9fceaa3
Cristobal Rosa <crisrogar@gmail.com>
2023-08-18 13:38:11 +0200

    feat: creacion microservicio cliente paises

---
+++ microservicio_cliente_paises/src/test/java/com/jcaido/microservicio_cliente_paises/MicroservicioClientePaisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcaido.microservicio_cliente_paises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioClientePaisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
