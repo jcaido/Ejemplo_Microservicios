79b67c34f0f6c9c90b8bbac7dfb569a1736243e1
Cristobal Rosa <crisrogar@gmail.com>
2023-08-24 10:38:51 +0200

    feat: configuracion feign microservicio cliente contactos feign

--- microservicio_cliente_contactos_feign/src/main/java/com/jcaido/microservicio_cliente_contactos_feign/MicroservicioClienteContactosFeignApplication.java
+++ microservicio_cliente_contactos_feign/src/main/java/com/jcaido/microservicio_cliente_contactos_feign/MicroservicioClienteContactosFeignApplication.java
@@ -2,8 +2,10 @@ package com.jcaido.microservicio_cliente_contactos_feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MicroservicioClienteContactosFeignApplication {
 
 	public static void main(String[] args) {
