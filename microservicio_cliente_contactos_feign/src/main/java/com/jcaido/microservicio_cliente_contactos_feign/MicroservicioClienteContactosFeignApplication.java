1d0746c50a6f019cf2bf3995d8d13b12938cf3e4
Cristobal Rosa <crisrogar@gmail.com>
2023-08-24 10:15:34 +0200

    feat: creacion microservicio cliente contactos feign

---
+++ microservicio_cliente_contactos_feign/src/main/java/com/jcaido/microservicio_cliente_contactos_feign/MicroservicioClienteContactosFeignApplication.java
@@ -0,0 +1,13 @@
+package com.jcaido.microservicio_cliente_contactos_feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioClienteContactosFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioClienteContactosFeignApplication.class, args);
+	}
+
+}
---
+++ microservicio_cliente_contactos_feign/src/test/java/com/jcaido/microservicio_cliente_contactos_feign/MicroservicioClienteContactosFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcaido.microservicio_cliente_contactos_feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioClienteContactosFeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
