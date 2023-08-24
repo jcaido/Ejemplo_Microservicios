0509ea3b6a1ef26b9460b740ebee739fba4b7bf0
Cristobal Rosa <crisrogar@gmail.com>
2023-08-24 10:50:40 +0200

    feat: configuracion interface PersonasFeign microservicio cliente contactos feign

---
+++ microservicio_cliente_contactos_feign/src/main/java/com/jcaido/microservicio_cliente_contactos_feign/config/PersonasFeign.java
@@ -0,0 +1,20 @@
+package com.jcaido.microservicio_cliente_contactos_feign.config;
+
+import com.jcaido.microservicio_cliente_contactos_feign.model.Persona;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(value = "servicio-contactos")
+public interface PersonasFeign {
+
+    @GetMapping(value = "contactos", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<Persona> getPersonal();
+
+    @PostMapping(value = "contactos", consumes = MediaType.APPLICATION_JSON_VALUE)
+    void altaPersona(@RequestBody Persona persona);
+}
