9e8ed9c0b2257e5d95d41f76ee9202ba843cbaac
Cristobal Rosa <crisrogar@gmail.com>
2023-08-24 11:01:02 +0200

    feat: alta personas PersonasController microservicio cliente contactos feign

---
+++ microservicio_cliente_contactos_feign/src/main/java/com/jcaido/microservicio_cliente_contactos_feign/controller/PersonasController.java
@@ -0,0 +1,29 @@
+package com.jcaido.microservicio_cliente_contactos_feign.controller;
+
+import com.jcaido.microservicio_cliente_contactos_feign.config.PersonasFeign;
+import com.jcaido.microservicio_cliente_contactos_feign.model.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PersonasController {
+
+    @Autowired
+    PersonasFeign personasFeign;
+
+    @PostMapping(value = "personas/{nombre}/{email}/{edad}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Persona> altaPersonas(
+            @PathVariable String nombre,
+            @PathVariable String email,
+            @PathVariable Integer edad) {
+        Persona persona = new Persona(nombre, email, edad);
+        personasFeign.altaPersona(persona);
+
+        return personasFeign.getPersona();
+    }
+}
